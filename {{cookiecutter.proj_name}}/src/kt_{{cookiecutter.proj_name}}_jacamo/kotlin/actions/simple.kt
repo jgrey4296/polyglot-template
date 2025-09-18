8861abc1ebb6d1b9c0d2d86c700fca5dda89d6e0
John Grey <johngrey4296@gmail.com>
2025-09-18 05:14:51 +0100

    [chore]: env setup and kotlin linting

---
+++ {{cookiecutter.proj_name}}/src/kt_{{cookiecutter.proj_name}}_jacamo/kotlin/actions/simple.kt
@@ -0,0 +1,27 @@
+//  jg_action.kt -*- mode: Kotlin -*-
+@file:JvmName("simple")
+
+package jg.actions
+
+import jason.asSemantics as JsSem
+import jason.asSyntax as JsSyn
+import kotlin.text
+
+public class SimpleAction : DefaultInternalAction() {
+    val subspendIntention   = false
+    val canBeUsedInContext  = false
+
+    init {
+        println("Making SimpleAction")
+    }
+
+    /** Prepare body's terms to be used in 'execute', normally it consist of cloning and applying each term */
+    // override public prepareArguments(Literal body, Unifier un) : Array<Term> {}
+
+    @Throws(Exception::class)
+    public override fun execute(ts:TransitionSystem, un:Unifier, args:Array<Term>): Boolean {
+        ts.logger.info("My Example Action")
+        Thread.sleep(2000)
+        return true
+    }
+}
