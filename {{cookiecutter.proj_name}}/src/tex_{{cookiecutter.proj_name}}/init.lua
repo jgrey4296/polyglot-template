83116e7b049798f33d72084e4524a3e7d11c51d9
John Grey <johngrey4296@gmail.com>
2025-10-01 20:00:58 +0100

    [incremental]: latex

---
+++ {{cookiecutter.proj_name}}/src/tex_{{cookiecutter.proj_name}}/init.lua
@@ -0,0 +1,20 @@
+-- ## init.lua -*- mode: lua-mode -*-
+-- # Summary:
+-- #
+-- # Tags:
+-- #
+local getlocal  = require("debug" ).getlocal
+local texio     = require("texio")
+local status    = require("status")
+local token     = require("token")
+
+function write_to_aux ()
+  info = status.list()
+  base_path = string.format("%s.aux", string.sub(info.filename, 0, -5))
+  texio.write_nl("term", string.format("Writing to Aux: %s", base_path))
+  auxf = io.open(base_path,"a+")
+  if auxf == nil then return end
+  auxf:write([[blah blah blah]])
+  -- f:write([[\def\gtt@chartextrasize#1#2{}]])
+  auxf:close()
+end
