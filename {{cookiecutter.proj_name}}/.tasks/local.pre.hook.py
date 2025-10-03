35ac5d6b08eb1a9dd565dd24478df54198647f4e
John Grey <johngrey4296@gmail.com>
2025-10-03 16:28:49 +0100

    [add]: pre-commit

---
+++ {{cookiecutter.proj_name}}/.tasks/local.pre.hook.py
@@ -0,0 +1,6 @@
+# basic.local.py -*- mode: python -*-
+
+import sys
+print("Basic Local Python hook: ", sys.argv[1])
+
+sys.exit(0)
---
+++ {{cookiecutter.proj_name}}/.tasks/local.pre.hook.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# test-hook2.sh -*- mode: sh -*-
+# exit on various failures:
+
+echo "Basic Local Shell Hook: $@"
+
+exit 0
