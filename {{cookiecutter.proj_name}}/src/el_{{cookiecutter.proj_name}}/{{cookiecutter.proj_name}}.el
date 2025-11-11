69a2411b739aa8b237c59d432e35f7ff65405da6
John Grey <johngrey4296@gmail.com>
2025-11-11 13:28:31 +0000

    [add]: lisp / eask

---
+++ {{cookiecutter.proj_name}}/src/el_{{cookiecutter.proj_name}}/{{cookiecutter.proj_name}}.el
@@ -0,0 +1,21 @@
+;;; {{cookiecutter.proj_name}}.el --- -*- lexical-binding: t; no-byte-compile: t; -*-
+;;;
+;; Copyright (C) 2025 john
+;;
+;; Author: john <https://github.com/jgrey4296>
+;; Created: November 11, 2025
+;; Modified: November 11, 2025
+;; Version: 0.1.0
+;; Keywords:
+;; Homepage: https://github.com/jgrey4296/{{cookiecutter.proj_name}}
+;; Package-Requires: ((emacs "30.2"))
+;; Package written on: ((emacs 30.2))
+;;
+;; This file is not part of GNU Emacs.
+;;
+
+;;; Commentary:
+
+
+(provide '{{cookiecutter.proj_name}})
+;;; {{cookiecutter.proj_name}}.el ends here
