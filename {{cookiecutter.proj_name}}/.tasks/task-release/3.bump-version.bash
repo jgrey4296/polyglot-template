39eec5016af521a7e4a5ca25ee50979cea344307
John Grey <johngrey4296@gmail.com>
2025-11-25 08:23:24 +0000

    [chore]:

--- {{cookiecutter.proj_name}}/.tasks/task-release/3.bump-version.bash
+++ {{cookiecutter.proj_name}}/.tasks/task-release/3.bump-version.bash
@@ -4,5 +4,6 @@ set -euo pipefail
 subhead "Calculating Version Number"
 CURR_VERSION=$(version version)
 version "$LEVEL" "set" "+"
+version file update-all
 NEW_VERSION=$(version version)
 echo "Project Version $CURR_VERSION -> $NEW_VERSION"
