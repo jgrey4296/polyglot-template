bc77585bab388ec71515043923b35d9d52b7036c
John Grey <johngrey4296@gmail.com>
2025-12-02 01:05:23 +0000

    [refactor]: rs_bin -> rs_exe

---
+++ {{cookiecutter.proj_name}}/src/rs_exe/src/main.rs
@@ -0,0 +1,10 @@
+// __main.rs -*- mode: rustic -*-
+//
+//
+//
+use {{cookiecutter.proj_name}}_lib_rs;
+
+fn main() {
+    println!("Hello, world! blah");
+
+}
---
+++ {{cookiecutter.proj_name}}/src/rs_exe/tests/basic_tests.rs
@@ -0,0 +1,13 @@
+// __ -*- mode: rustic -*-
+//
+//
+//
+use {{cookiecutter.proj_name}}_rs;
+// //--// tests
+#[cfg(test)]
+#[test]
+fn test_basic() {
+    assert!(true);
+}
+
+// //--// end tests
